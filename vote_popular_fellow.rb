f514ddfcf3800465899f6fb11ad12c79bd23aeb4
tensaix2j <tensaix2j@tensaix2j-VirtualBox.(none)>
2017-09-04 00:34:59 +0800

    modified:   vote_popular_fellow.rb

--- vote_popular_fellow.rb
+++ vote_popular_fellow.rb
@@ -7,18 +7,24 @@ require 'radiator'
 def vote( author, voter, voter_wif , permlink )  
 
 	puts "#{voter} voting for #{author}"
-	tx = Radiator::Transaction.new(wif: voter_wif)
-	vote = {
-	  type: :vote,
-	  voter: voter,
-	  author: author,
-	  permlink: permlink,
-	  weight: 10000
-	}
 
-	tx.operations << vote
-	tx.process(true)
+	begin
+		tx = Radiator::Transaction.new(wif: voter_wif)
+		vote = {
+		  type: :vote,
+		  voter: voter,
+		  author: author,
+		  permlink: permlink,
+		  weight: 10000
+		}
 
+		tx.operations << vote
+		tx.process(true)
+
+	rescue Exception => ex
+		puts "\n Failed to vote for #{author}"
+		puts ex.to_s
+	end	
 end
 
 
@@ -56,6 +62,7 @@ def main
 		if op.parent_author == "" && @popular_fellows.index(op.author) != nil
 			@voters.each { |v|
 				vote( op.author, v["user"], v["wif"], op.permlink )
+				sleep 1
 			}
 		end
 	end
