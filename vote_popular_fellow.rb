b0644f336d0980b9c189e1ac1b60c2278e2d885e
JJ <JJ>
2017-09-04 09:53:11 +0800

    modified:   vote_popular_fellow.rb

--- vote_popular_fellow.rb
+++ vote_popular_fellow.rb
@@ -1,6 +1,7 @@
 
 require 'rubygems'
 require 'radiator'
+require 'time'
 
 
 #-----------------------------------
@@ -48,7 +49,34 @@ end
 
 
 
+#-------------------------
+def process_votequeue
+
+	printf "#{@vote_queue.count} "
+	if @vote_queue.length > 0
+
+		votable = @vote_queue[0]
+		comment = api.get_content(votable[:author], votable[:permlink]).result
+
+		post_epoch = Time.parse( comment.created ).to_i 
+		now_epoch  = Time.now.to_i 
+
+		# 30 minutes rule
+		if now_epoch - post_epoch > 1500 && now_epoch - post_epoch < 2400
+			@voters.each { |v|
+				vote( votable[:author], v["user"], v["wif"], votable[:permlink] )
+				sleep 1
+			}
+			@vote_queue.shift!
+		end	
+	end				
+end
+
+
+
+
 #---------------------
+@vote_queue = []
 def main
 		
 	populate_voters
@@ -56,14 +84,30 @@ def main
 	p @voters 
 	p @popular_fellows
 
-	stream = Radiator::Stream.new
-	stream.operations(:comment) do |op|
-  
-		if op.parent_author == "" && @popular_fellows.index(op.author) != nil
-			@voters.each { |v|
-				vote( op.author, v["user"], v["wif"], op.permlink )
-				sleep 1
-			}
+	loop do
+		
+		stream 	= Radiator::Stream.new
+		api  	= Radiator::Api.new
+		
+		begin 
+			
+			stream.operations(:comment) do |op|
+		  			
+				if op.parent_author == "" && @popular_fellows.index(op.author) != nil
+					votable = {}
+					votable[:author] 	= op.author 
+					votable[:permlink] 	= op.permlink
+					vote_queue << votable
+				end
+
+
+				process_votequeue
+
+			end
+		rescue Exception => ex
+			puts "Error in stream.operation. Error: #{ ex.to_s }. Retrying.."
+			puts ex.backtrace
+			sleep 5
 		end
 	end
 end
